9f1b6483c0dff66656cbde128ead5afca46b04e4
Leo Allesch <leoallesch@gmail.com>
2026-02-07 18:51:30 -0500

    first commit

---
+++ file_list_gen.cmake
@@ -0,0 +1,6 @@
+list(
+  APPEND
+  PROJECT_SOURCES
+  ${CMAKE_CURRENT_LIST_DIR}/screens/hello_world_gen.c
+  ${CMAKE_CURRENT_LIST_DIR}/ui_hello_world_gen.c
+  ${CMAKE_CURRENT_LIST_DIR}/ui_hello_world.c)
